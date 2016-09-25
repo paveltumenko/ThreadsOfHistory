acee34f19385ee3bb56e254c4a6debe583852da8
paveltumenko <pavel.tumenko@gmail.com>
2016-09-25 22:58:06 +0300

    Adding EventsController

---
+++ ThreadsOfHistory/ThreadsOfHistory/Controllers/EventsController.cs
@@ -0,0 +1,120 @@
+﻿using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using DataAccess;
+using Models;
+
+namespace ThreadsOfHistory.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly IRepository<Event> _repository;
+
+        public EventsController(IRepository<Event> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: Events
+        public async Task<ActionResult> Index()
+        {
+            return View(await _repository.GetAsync());
+        }
+
+        // GET: Events/Details/5
+        public async Task<ActionResult> Details(decimal id)
+        {
+            if (id < 1)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var @event = await _repository.GetAsync(id);
+            if (@event == null)
+                return HttpNotFound();
+
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Events/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Description,Scale,StartDate,EndDate")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.Add(@event);
+                await _repository.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(@event);
+        }
+
+        // GET: Events/Edit/5
+        public async Task<ActionResult> Edit(decimal id)
+        {
+            if (id < 1)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var @event = await _repository.GetAsync(id);
+            if (@event == null)
+                return HttpNotFound();
+
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Description,Scale,StartDate,EndDate")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.Update(@event);
+                await _repository.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public async Task<ActionResult> Delete(decimal id)
+        {
+            if (id < 1)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var @event = await _repository.GetAsync(id);
+            if (@event == null)
+                return HttpNotFound();
+
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(decimal id)
+        {
+            var @event = await _repository.GetAsync(id);
+            _repository.Remove(@event);
+            await _repository.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
