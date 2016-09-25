5f8a185988c275b47818b705743bbc7c95612f21
paveltumenko <pavel.tumenko@gmail.com>
2016-09-25 22:51:49 +0300

    Adding EventRepository

---
+++ ThreadsOfHistory/DataAccess/EventRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Models;
+
+namespace DataAccess
+{
+    public class EventRepository : IRepository<Event>
+    {
+        private readonly ThreadsOfHistoryContext _context;
+
+        public EventRepository()
+        {
+            _context = new ThreadsOfHistoryContext();
+        }
+
+        public Task<Event> GetAsync(decimal id)
+        {
+            return _context.Events.FindAsync(id);
+        }
+
+        public Task<List<Event>> GetAsync()
+        {
+            return _context.Events.ToListAsync();
+        }
+
+        public void Add(Event entity)
+        {
+            _context.Events.Add(entity);
+        }
+
+        public void Update(Event entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Remove(Event entity)
+        {
+            _context.Events.Remove(entity);
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
---
+++ ThreadsOfHistory/DataAccess/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+
+namespace DataAccess
+{
+    public interface IRepository<T> : IDisposable where T : BaseModel
+    {
+        Task<Event> GetAsync(decimal id);
+        Task<List<Event>> GetAsync();
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        Task<int> SaveChangesAsync();
+    }
+}
